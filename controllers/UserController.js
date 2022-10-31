033ad94de8c78e778bef2c78682e6925cfd896c5
SergioJ-Vidal <113844469+SergioJ-Vidal@users.noreply.github.com>
2022-10-31 15:22:32 +0100

    BD creada, Trabajando en tablas

---
+++ controllers/UserController.js
@@ -0,0 +1,19 @@
+const { User } = require('../models/index.js');
+
+const UserController = {
+
+    create(req, res) {
+    
+    req.body.role = "user";
+    
+    User.create(req.body)
+    
+    .then(user => res.status(201).send({ message: 'User Created', user }))
+    
+    .catch(console.error)
+    
+    },
+    
+    }
+    
+    module.exports = UserController
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000
+
+app.use(express.json())
+
+app.use('/users', require('./routes/users'));
+
+app.listen(PORT, () => console.log('Servidor levantado en el puerto ' + PORT))
